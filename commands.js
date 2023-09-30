7122f02a7d704a069eee5e4268e15d455c43efea
gb-redRabbit <136236629+gb-redRabit@users.noreply.github.com>
2023-09-30 19:24:57 +0200

    Initial commit

---
+++ commands.js
@@ -0,0 +1,67 @@
+const password = "rabbit";
+const linkedin = "https://www.linkedin.com/in/grzegorz-bejtan-bb4a67198/";
+const github = "https://github.com/gb-redRabit";
+const email = "mailto:bejtan.grzegorz@gmail.com";
+
+whoami = ["<br>", "Hey, I'm Grzegorz!👋", "Coś tam dalej", "<br>"];
+
+social = [
+  "<br>",
+  `linkedin       <a class="link" href="${linkedin}" target="_blank">linkedin/grzegorzBejtan</a>`,
+  `github         <a class="link" href="${github}" target="_blank">github/gb-redRabit</a>`,
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span><span class="link">Only use if you\'re admin</span>',
+  "<br>",
+];
+
+projects = [];
+list = [];
+fetch("https://api.github.com/users/gb-redRabit/repos")
+  .then((res) => res.json())
+  .then((res) => {
+    res.map((item) => {
+      if (item.name != "gb-redRabit") {
+        list.push({ name: item.name, url: item.clone_url });
+        projects.push(
+          `${item.name}<a class="link" href="${item.clone_url}" target="_blank">github/${item.name}</a>`
+        );
+      }
+    });
+  });
+help = [
+  "<br>",
+  '<span class="command">whoami</span><span class="link">Who am I?</span>', //dokończyć opis
+  '<span class="command">social</span><span class="link">Display social networks</span>', // pozmieniąć linki
+  '<span class="command">secret</span><span class="link">Find the password</span>', // rozbudować sekret dodać efekt wirusa
+  '<span class="command">projects</span><span class="link">View coding projects</span>', // stworzyć fukcje wipisujaca projekty
+  '<span class="command">cv</span><span class="link">Download CV</span>', //dodać
+  '<span class="command">history</span><span class="link">View command history</span>',
+  '<span class="command">help</span> <span class="link"> You obviously already know what this does</span>',
+  '<span class="command">email</span><span class="link">Write to me</span>',
+  '<span class="command">clear</span><span class="link">Clear terminal</span>',
+  '<span class="command">banner</span><span class="link">Display the header</span>',
+  "<br>",
+];
+
+banner = [
+  // cos pozmieniać??????????
+  '<span class="index">gb-redRabit (GB) </span>',
+  "                  _",
+  "                 /_\\",
+  "                 \\\\\\\\     ___          _____________________________________",
+  "                  \\\\\\\\   /   \\        /                                     \\",
+  "                   \\\\\\\\_/ \\   \\      /     What does a computer scientist    \\",
+  "                    \\     /\\__/     /   do when he wakes up with a hangover?  \\",
+  "                     \\X -/          \\               Tests memory.             /",
+  "                  ___/ ^ \\___       / _______________________________________/",
+  "                     \\___/         /_/",
+  "                     _/ \\_",
+  "                  __//   \\\\__",
+  "                 /___\\/_\\/___\\",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
---
+++ main.js
@@ -0,0 +1,229 @@
+let before = document.getElementById("before");
+const liner = document.getElementById("liner"),
+  command = document.getElementById("typer"),
+  textarea = document.getElementById("texter"),
+  terminal = document.getElementById("terminal"),
+  cursor = document.getElementById("cursor");
+
+let git = 0,
+  pw = false,
+  pwd = false,
+  commands = [];
+
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+setTimeout(function () {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", (e) => {
+  enterKey(e);
+});
+window.addEventListener("load", () => {
+  cursor.style.left = "0px";
+});
+
+console.log(
+  "%cYou hacked my password!😠",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+console.log(
+  "%cPassword: '" + password + "' - I wonder what it does?🤔",
+  "color: grey"
+);
+
+const enterKey = (e) => {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("gb-redRabit:" + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    help;
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+};
+
+const commander = (cmd) => {
+  cursor.style.left = "0px";
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "video":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "sudo":
+      addLine("Oh no, you're not admin...", "color2", 80);
+      setTimeout(() => {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+      }, 1000);
+
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "secret":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "projects":
+      addLine("<br/>", "color2", 0);
+      loopLines(projects, "color2 margin project", 80);
+      addLine("<br/>", "color2", list.length * 80 + 80);
+      break;
+    case "password":
+      addLine(
+        "<span class=\"inherit\"> Lol! You're joking, right? You're gonna have to try harder than that!😂</span>",
+        "error",
+        100
+      );
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine(
+        'Opening mailto:<a href="mailto:bejtan.grzegorz@gmail.com"> bejtan.grzegorz@gmail.com</a>...',
+        "color2",
+        80
+      );
+      newTab(email);
+      break;
+    case "clear":
+      setTimeout(() => {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    case "cv":
+      addLine("Download  CV...", "color2", 0);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      const item = list.filter((item) => item.name == cmd.toLowerCase());
+      if (item.length > 0) {
+        addLine(`Opening GitHub/${item[0].name}...`, "color2", 0);
+        newTab(item[0].url);
+      } else {
+        addLine(
+          '<span class="inherit">Command not found. For a list of commands, type <span class="command">\'help\'</span>.</span>',
+          "error",
+          100
+        );
+      }
+      break;
+  }
+};
+
+const newTab = (link) => {
+  setTimeout(() => {
+    window.open(link, "_blank");
+  }, 500);
+};
+
+const addLine = (text, style, time) => {
+  let t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(() => {
+    let next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+};
+
+const loopLines = (name, style, time) => {
+  name.forEach(function (item, index) {
+    addLine(item, style, index * time);
+  });
+};
+
+const typeIt = (from, e) => {
+  e = e || window.event;
+  let w = document.getElementById("typer");
+  let tw = from.value;
+  if (!pw) {
+    w.innerHTML = tw.replace(/\n/g, "");
+  }
+};
+const moveIt = (count, e) => {
+  e = e || window.event;
+  let keycode = e.keyCode || e.which;
+  if (keycode == 37 && parseInt(cursor.style.left) >= 0 - (count - 1) * 10) {
+    cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
+  } else if (keycode == 39 && parseInt(cursor.style.left) + 10 <= 0) {
+    cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+  }
+};
